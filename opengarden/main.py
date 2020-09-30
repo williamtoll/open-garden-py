07e6e582e5dbed25205241ea5665703c360c0bb7
wtoledo <wtoledo@roshka.com>
2020-09-30 17:41:33 -0400

    Hello World Im the First Commit,

---
+++ opengarden/main.py
@@ -0,0 +1,44 @@
+# This Python file uses the following encoding: utf-8
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtWidgets import QApplication, QDialog, QVBoxLayout, QLabel
+from PyQt5.QtGui import QPixmap
+from pyqtgraph import PlotWidget, plot
+import pyqtgraph as pg
+import sys  # We need sys so that we can pass argv to QApplication
+import os
+
+
+class MainWindow(QtWidgets.QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.title = "OpenGarden"
+        self.top = 200
+        self.left = 500
+        self.width = 400
+        self.height = 300
+
+
+
+        #Load the UI Page
+        uic.loadUi('mainwindow.ui', self)
+        self.InitWindow()
+
+
+
+
+    def InitWindow(self):
+        pixmap = QPixmap("home garden.jpg")
+        self.labelImage.setPixmap(pixmap)
+
+
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    main = MainWindow()
+    main.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+        main()
